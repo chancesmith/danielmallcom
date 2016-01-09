e3b8df26c2ee032d8f53adf689799ac63631c4cf
DANIEL MALL <danielmall@dan.local.home>
2016-01-09 16:22:50 -0500

    Homepage HTML, first draft done

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
         <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
     <![endif]-->
 
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/nav.php"); echo "\n"; ?>
+
     <section id="preamble">
 
 	    <h1>Dan Mall</h1>
