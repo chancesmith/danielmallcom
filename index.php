829ed2dd25ff38bfb84f693080ba8c80c6282a22
DANIEL MALL <danielmall@dan.local.home>
2016-01-09 16:12:18 -0500

    Homepage markup started

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
 <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
 <head>    
-    <title></title>
+    <title>Dan Mall / Creative Director &amp; Advisor</title>
     <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?>        
 
     <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>        
@@ -11,5 +11,57 @@
     <!--[if lt IE 7]>
         <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
     <![endif]-->
+
+    <section id="preamble">
+
+	    <h1>Dan Mall</h1>
+	    <h2>Creative Director &amp; Advisor</h2>
+
+	    <p class="intro">I&rsquo;m <strong>Dan Mall</strong>, a creative director and advisor from Philly.  I&rsquo;m the director of <a href="http://superfriend.ly/">SuperFriendly</a>, a design collaborative that brings exquisite creative direction &amp; design to the world&rsquo;s most important and interesting organizations. I&rsquo;m an enthralled husband &amp; dad and co-founder of <a href="http://typedia.com/">Typedia</a> (an encyclopedia for typefaces) and <a href="http://businessology.biz/">Businessology</a> (a podcast and workshop series teaching designers how to run better businesses). You can find me on Twitter at <a href="http://twitter.com/danielmall">@danielmall</a>.</p>
+
+	</section><!-- #preamble -->
+
+    <section id="pd-book">
+
+    	<h1>I wrote a book.</h1>
+
+    	<p>Curabitur blandit tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus.</p>
+    	<p>Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo.</p>
+
+    	<!-- todo: insert PD book image with AJAX -->
+
+    </section><!-- #pd-book -->
+
+    <section id="testimonials">
+
+    	<blockquote>
+    		<p>Few designers can capture a brand&rsquo;s essence and deliver a design that takes that brand to a new level. Dan did that at the pitch meeting.</p>
+			<address><strong>Rob Walker</strong>, Former Digital Marketing Manager, Crayola</address>
+    	</blockquote>
+
+    	<blockquote>
+    		<p>I promise that Dan Mall is more inspiring and talented than you.</p>
+			<address><strong>Chris Matthews</strong>, Strategy, Apple</address>
+    	</blockquote>
+
+    	<blockquote>
+    		<p>Dan is an amazing teacher and mentor to everyone he works with&mdash;no matter the discipline or level. My only regret about working with Dan is that it didn&rsquo;t last longer.</p>
+			<address><strong>Karina Elise</strong>, Creative Lead, Google</address>
+    	</blockquote>
+
+    	<blockquote>
+    		<p>Dan certainly has one of the best minds for design in our industry, but he takes it so much further in his unparalleled grace and his seamless collaboration with his team and his clients.</p>
+			<address><strong>Mike Kenny</strong>, Director of User Experience, Code &amp; Theory</address>
+    	</blockquote>
+
+    </section><!-- #testimonials -->
+
+    <section id="articles">
+
+    	<h1>Latest Articles</h1>
+
+    	<p><a href="/articles/">See all articles</a></p>
+
+    </section><!-- #articles -->
 
 <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
